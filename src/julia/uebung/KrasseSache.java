d11ebd4980d6f3e94a26316b836278f2ecf18b2c
J.Weidehaas <J.Weidehaas@5-6--4.Bildung.zz>
2016-05-26 15:07:38 +0200

    irgendwas

---
+++ src/julia/uebung/KrasseSache.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package julia.uebung;
+
+/**
+ *
+ * @author J.Weidehaas
+ */
+public class KrasseSache {
+    public static void main(String[] args) {
+        // Kommentar
+    }
+}
